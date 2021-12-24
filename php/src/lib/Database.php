271d76b61c55607228ff722f4b7b1a47a8512a6c
UMINURHIDAYATI <uminurhidayati012@gmail.com>
2021-12-24 10:37:54 +0700

    UAS

--- php/src/lib/Database.php
+++ php/src/lib/Database.php
@@ -1,9 +1,9 @@
 <?php
 Class Database{
-	public $host   = "10.0.0.76";
-	public $user   = "admin";
-	public $pass   = "Hidayati012.";
-	public $dbname = "kuis";
+	public $host   = DB_HOST;
+	public $user   = DB_USER;
+	public $pass   = DB_PASS;
+	public $dbname = DB_NAME;
 	
 	
 	public $link;
